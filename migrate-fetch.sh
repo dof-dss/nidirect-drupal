02cb75e8820fc6d9089e2ad6073b23f168e8cd4b
Johan Gant <johan.gant@gmail.com>
2020-07-21 10:16:19 +0100

    D8NID-796 migrations on psh (#388)
    * Add extra mariabdb service to house legacy content
    
    * Configure migrate db
    
    * Add more disk space for migrate db
    
    * DB size bump
    
    * Enable migrate modules
    
    * Jig about the reference to drupal7db
    
    * Increase application storage to accommodate migration data files
    
    * Add support scripts to fetch and unpack migration assets, and setup
    crons to run them at key times
    
    * Use env var to represent source location
    
    * Typo in var reference
    
    * Ensure we only execute migrate fetch/pack tasks on environments where we designate a specific variable

---
+++ migrate-fetch.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+if [ ! $MIGRATE_ENABLED == 1 ]; then
+  exit 0
+fi
+
+curl -u ${AZURE_HTTP_USER}:${AZURE_HTTP_PASS} -X GET -o /app/imports/nidirectd7.sql.gz "${AZURE_HOST}/files/nidirect.sql.gz"
+curl -u ${AZURE_HTTP_USER}:${AZURE_HTTP_PASS} -X GET -o /app/imports/nidirect_files.tar "${AZURE_HOST}/files/nidirect_files_change.tar"
---
+++ migrate-unpack.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+if [ ! $MIGRATE_ENABLED == 1 ]; then
+  exit 0
+fi
+
+echo "Restore /app/imports/nidirectd7.sql.gz into drupal7db database..."
+gunzip /app/imports/nidirectd7.sql.gz
+drush -r /app/web sqlc --database=drupal7db < /app/imports/nidirectd7.sql
+gzip /app/imports/nidirectd7.sql && mv /app/imports/nidirectd7.sql.gz /app/imports/nidirectd7.last.sql.gz
+echo ".... DONE"
+
+echo "Extract file assets into migrate-import directory..."
+cd /app/imports
+tar -xf nidirect_files.tar
+rsync -avzh /app/imports/opt/rh/httpd24/root/var/www/html/drupal_sites/nidirect/files/ /app/imports/files/sites/default/files/
+rm -rf /app/imports/opt
+echo ".... DONE"
