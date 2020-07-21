7891a849172f2b33c511cd3ba1145534a46523fa
Johan Gant <johan.gant@gmail.com>
2020-07-21 09:32:53 +0100

    Ensure we only execute migrate fetch/pack tasks on environments where we designate a specific variable

--- migrate-unpack.sh
+++ migrate-unpack.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/bash
+if [ ! $MIGRATE_ENABLED == 1 ]; then
+  exit 0
+fi
+
 echo "Restore /app/imports/nidirectd7.sql.gz into drupal7db database..."
 gunzip /app/imports/nidirectd7.sql.gz
 drush -r /app/web sqlc --database=drupal7db < /app/imports/nidirectd7.sql
