82d3bea5ac504b70857d5c62c53ec2ece87a54e8
Johan Gant <johan.gant@gmail.com>
2020-09-08 16:36:03 +0100

    Remove legacy scripts and cron handlers

--- migrate-fetch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ ! $MIGRATE_ENABLED == 1 ]; then
-  exit 0
-fi
-
-curl -u ${AZURE_HTTP_USER}:${AZURE_HTTP_PASS} -X GET -o /app/imports/nidirectd7.sql.gz "${AZURE_HOST}/files/nidirect.sql.gz"
-curl -u ${AZURE_HTTP_USER}:${AZURE_HTTP_PASS} -X GET -o /app/imports/nidirect_files.tar "${AZURE_HOST}/files/nidirect_files_change.tar"
