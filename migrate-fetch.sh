ad7de888593c7641c3a49ebd7e93b9e72af6ada5
Johan Gant <johan.gant@gmail.com>
2020-09-09 12:21:22 +0100

    Accept file overwrite by default

--- migrate-fetch.sh
+++ migrate-fetch.sh
@@ -6,9 +6,5 @@ fi
 PLATFORM_LEGACY_PROJECT_ID=$1
 PLATFORM_LEGACY_BRANCH=$2
 
-if [ -f /app/imports/nidirectd7.sql.gz ]; then
-  rm /app/imports/nidirectd7.sql.gz
-fi
-
-platform db:dump -p $PLATFORM_LEGACY_PROJECT_ID -e $PLATFORM_LEGACY_BRANCH --gzip -f /app/imports/nidirectd7.sql.gz
+platform db:dump -p $PLATFORM_LEGACY_PROJECT_ID -e $PLATFORM_LEGACY_BRANCH --gzip -f /app/imports/nidirectd7.sql.gz -y
 platform mount:download $PLATFORM_LEGACY_PROJECT_ID -e $PLATFORM_LEGACY_BRANCH -m "/public_html/sites/default/files" --exclude "css*" --exclude "js*" --exclude="status_check*" --target /app/imports/files/sites/default/files/ -y
