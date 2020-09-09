a095d250fa9b13574e5fd7d4d74543cd34f9ffd4
Johan Gant <johan.gant@gmail.com>
2020-09-09 12:32:20 +0100

    Typo

--- migrate-fetch.sh
+++ migrate-fetch.sh
@@ -7,4 +7,4 @@ PLATFORM_LEGACY_PROJECT_ID=$1
 PLATFORM_LEGACY_BRANCH=$2
 
 #platform db:dump -p $PLATFORM_LEGACY_PROJECT_ID -e $PLATFORM_LEGACY_BRANCH --gzip -f /app/imports/nidirectd7.sql.gz -y
-platform mount:download $PLATFORM_LEGACY_PROJECT_ID -e $PLATFORM_LEGACY_BRANCH -m "/public_html/sites/default/files" --exclude "css*" --exclude "js*" --exclude="status_check*" --target /app/imports/files/sites/default/files/ -y
+platform mount:download -p $PLATFORM_LEGACY_PROJECT_ID -e $PLATFORM_LEGACY_BRANCH -m "/public_html/sites/default/files" --exclude "css*" --exclude "js*" --exclude="status_check*" --target /app/imports/files/sites/default/files/ -y
