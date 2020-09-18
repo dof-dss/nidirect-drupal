aba14d75ec5b58ba94410f23cc21697409c38cd0
Johan Gant <johan.gant@gmail.com>
2020-09-18 14:27:20 +0100

    Force gzip to overwrite file if it already exists

--- migrate-unpack.sh
+++ migrate-unpack.sh
@@ -6,5 +6,5 @@ fi
 echo "Restore /app/imports/nidirectd7.sql.gz into drupal7db database..."
 gunzip /app/imports/nidirectd7.sql.gz
 drush -r /app/web sqlc --database=drupal7db < /app/imports/nidirectd7.sql
-gzip /app/imports/nidirectd7.sql && mv /app/imports/nidirectd7.sql.gz /app/imports/nidirectd7.last.sql.gz
+gzip -f /app/imports/nidirectd7.sql && mv /app/imports/nidirectd7.sql.gz /app/imports/nidirectd7.last.sql.gz
 echo ".... DONE"
