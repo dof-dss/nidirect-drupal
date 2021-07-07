9f6c7fec72d1226d4ce6552ffca187d396b06b15
omahm <hello@onemanandhismouse.com>
2021-07-07 12:32:42 +0100

    D8NID-1350 ~ Check the public filesystem is working propertly

--- web/healthcheck.php
+++ web/healthcheck.php
@@ -1,6 +1,7 @@
 <?php
 
 use Drupal\Core\DrupalKernel;
+use Drupal\Core\StreamWrapper\PublicStream;
 use Symfony\Component\HttpFoundation\Request;
 
 $autoloader = require_once 'autoload.php';
@@ -9,13 +10,12 @@ $kernel = DrupalKernel::createFromRequest(Request::createFromGlobals(), $autoloa
 $kernel->boot();
 
 $container = $kernel->getContainer();
-$database = $container->get('database');
-
 $errors = [];
 
 /**
  * Query the users table to verify that the database connection is active.
  */
+$database = $container->get('database');
 $result = $database->query("SELECT uid FROM {users} WHERE uid = 1")->fetch();
 
 if (empty($result->uid)) {
@@ -30,3 +30,17 @@ $maintenance_mode = $container->get('state')->get('system.maintenance_mode');
 if ($maintenance_mode) {
   $errors[] = 'Site offline: maintenance mode';
 }
+
+/**
+ * Check that the files directory is operating properly.
+ */
+$public_fs_path = PublicStream::basePath();
+
+if ($public_fs_path && $temp_file = tempnam($public_fs_path, 'status_check_')) {
+    if (!unlink($temp_file)) {
+      $errors[] = 'Could not delete newly created file in the files directory.';
+    }
+} else {
+    $errors[] = 'Could not create a file in the files directory.';
+}
+
