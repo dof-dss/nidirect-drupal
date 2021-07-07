94270f64bcf7b9c0f07e56cad877c242e4f0d48f
omahm <hello@onemanandhismouse.com>
2021-07-07 11:49:18 +0100

    D8NID-1350 ~ Query the users table

--- web/healthcheck.php
+++ web/healthcheck.php
@@ -7,5 +7,17 @@ $autoloader = require_once 'autoload.php';
 
 $kernel = DrupalKernel::createFromRequest(Request::createFromGlobals(), $autoloader, 'prod');
 $kernel->boot();
-$container = $kernel->getContainer();
 
+$container = $kernel->getContainer();
+$database = $container->get('database');
+
+$errors = [];
+
+/**
+ * Query the users table to verify that the database connection is active.
+ */
+$result = $database->query("SELECT uid FROM {users} WHERE uid = 1")->fetch();
+
+if (empty($result->uid)) {
+  $errors[] = 'Database query failed';
+}
