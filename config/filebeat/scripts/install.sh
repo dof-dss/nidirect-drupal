3bed6f7e7ef6db345852b9920ec9c97c8f852b9a
Neil Blair <neil.r.blair@gmail.com>
2022-02-24 16:47:40 +0000

    Tweaks

--- config/filebeat/scripts/install.sh
+++ config/filebeat/scripts/install.sh
@@ -7,13 +7,21 @@ TEMP_BEAT_HOME=config/filebeat/build
 [ ! -d $TEMP_BEAT_HOME ] && mkdir -p $TEMP_BEAT_HOME
 cd $TEMP_BEAT_HOME
 
+echo "Created ${TEMP_BEAT_HOME}"
+
 # Install Filebeat
 curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-8.0.0-linux-x86_64.tar.gz
 tar xzvf filebeat-8.0.0-linux-x86_64.tar.gz
 rm filebeat-8.0.0-linux-x86_64.tar.gz
 
+echo "Downloaded Filebeat"
+
 # Download the certificate
 wget https://raw.githubusercontent.com/logzio/public-certificates/master/AAACertificateServices.crt
 
+echo "Downloaded Logz.io certificate https://raw.githubusercontent.com/logzio/public-certificates/master/AAACertificateServices.crt"
+
 mkdir -p filebeat-8.0.0-linux-x86_64/pki/tls/certs
 cp AAACertificateServices.crt filebeat-8.0.0-linux-x86_64/pki/tls/certs/
+
+echo "Copied AAACertificateServices.crt to filebeat-8.0.0-linux-x86_64/pki/tls/certs/"
