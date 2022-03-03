107c831ff313d8d695a14830bf6181426547e24c
Neil Blair <neil.r.blair@gmail.com>
2022-03-03 09:13:21 +0000

    Always copy filebeat.yml into filebeat mount on every deploy

--- filebeat/scripts/config.sh
+++ filebeat/scripts/config.sh
@@ -3,5 +3,4 @@
 # Move filebeat to mount with write access
 cd $PLATFORM_HOME
 cp -r filebeat/build/filebeat-8.0.0-linux-x86_64/* .filebeat/
-cp filebeat/filebeat.yml .filebeat/
 mkdir .filebeat/registry
