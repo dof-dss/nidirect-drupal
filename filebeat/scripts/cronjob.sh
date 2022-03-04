edb5c4058ffca68909cf176027fa1e482d8728b9
Neil Blair <neil.r.blair@gmail.com>
2022-03-04 17:16:52 +0000

    Remove --once flag from filebeat start command because it causes filebeat to stop when EOF is reached on all log files.

--- filebeat/scripts/cronjob.sh
+++ filebeat/scripts/cronjob.sh
@@ -3,5 +3,5 @@
 # Run filebeat if not running already.
 if ! pgrep -x "filebeat" >/dev/null; then
   cd /app/.filebeat;
-  ./filebeat run --once &>/dev/null & disown;
+  ./filebeat run &>/dev/null & disown;
 fi
