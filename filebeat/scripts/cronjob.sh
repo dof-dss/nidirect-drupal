0eb29dcfb27d43ef16626caaf800eb4fbb323a3f
Neil Blair <neil.r.blair@gmail.com>
2022-04-05 17:48:25 +0100

    Some refactoring of cronjob script

--- filebeat/scripts/cronjob.sh
+++ filebeat/scripts/cronjob.sh
@@ -2,17 +2,19 @@
 
 #!/usr/bin/env bash
 
-cd /app/log/
-TODAY_DATE=$(date +%Y-%m-%d)
-YESTERDAY_DATE=$(date --date="yesterday" +%Y-%m-%d)
+# Ship today's latest log entries using Filebeat.
+#
+# Platform.sh truncates logs in /var/log to 100 MB. Filebeat cannot track
+# entries in truncated log files.  (See
+# https://www.elastic.co/guide/en/beats/filebeat/8.1/file-log-rotation.html)
+# This script, when run via cron, will periodically copy today's latest log
+# entries into a log file on a writeable mount. Filebeat can then harvest
+# this log file reliably because it is never truncated.
 
-# Delete yesterdays log file if it exists.
-YESTERDAY_LOG_FILE=${YESTERDAY_DATE}-access.log
-if [ -f ./${YESTERDAY_LOG_FILE} ]; then
-    rm ./${YESTERDAY_LOG_FILE}
-fi
+cd $LOGS_MOUNT_PATH
 
 # Create log file for today if it doesn't already exist.
+TODAY_DATE=$(date +%Y-%m-%d)
 TODAY_LOG_FILE=${TODAY_DATE}-access.log
 if [ ! -f ./${TODAY_LOG_FILE} ]; then
     touch ./${TODAY_LOG_FILE}
@@ -29,3 +31,10 @@ rm latest.log diff.log
 # Run filebeat to ship today's log (to remote logging service).
 cd /app/.filebeat
 ./filebeat run --once
+
+# Delete yesterdays log file if it exists.
+YESTERDAY_DATE=$(date --date="yesterday" +%Y-%m-%d)
+YESTERDAY_LOG_FILE=${YESTERDAY_DATE}-access.log
+if [ -f ./${YESTERDAY_LOG_FILE} ]; then
+    rm ./${YESTERDAY_LOG_FILE}
+fi
