aa959bcfb76994cc246e4ad36e2b0d7b0bb4afe7
Neil Blair <neil.r.blair@gmail.com>
2022-04-13 17:38:45 +0100

    Extend logging cronjob to upload log entries from drupal.log created by filelog module.

--- logging/scripts/cronjob.sh
+++ logging/scripts/cronjob.sh
@@ -14,35 +14,64 @@ cd $LOGS_MOUNT_PATH || exit
 
 # Create log file for today if it doesn't already exist.
 TODAY_DATE=$(date +%Y-%m-%d)
-TODAY_LOG_FILE=${TODAY_DATE}-access.log
-echo "> Creating log file for ${TODAY_DATE} if it does not exist ..."
-if [ ! -f ./${TODAY_LOG_FILE} ]; then
-    touch ./${TODAY_LOG_FILE}
-    echo "${TODAY_LOG_FILE} created"
+TODAY_ACCESS_LOG=${TODAY_DATE}-access.log
+TODAY_DRUPAL_LOG=${TODAY_DATE}-drupal.log
+
+echo "> Creating log files for ${TODAY_DATE} ..."
+
+if [ ! -f ./${TODAY_ACCESS_LOG} ]; then
+    touch ./${TODAY_ACCESS_LOG}
+    echo "${TODAY_ACCESS_LOG} created"
 else
-    echo "${TODAY_LOG_FILE} already exists"
+    echo "${TODAY_ACCESS_LOG} already exists"
+fi
+
+if [ ! -f ./${TODAY_DRUPAL_LOG} ]; then
+    touch ./${TODAY_DRUPAL_LOG}
+    echo "${TODAY_DRUPAL_LOG} created"
+else
+    echo "${TODAY_DRUPAL_LOG} already exists"
 fi
 
 # Compare latest access log entries with existing entries for today and add in newest entries.
-echo "> Retrieving latest log entries from /var/log/access.log and writing to ${TODAY_LOG_FILE}"
-cat /var/log/access.log | grep $(date +%d/%b/%Y:) > ./latest.log
-diff --changed-group-format='%>' --unchanged-group-format='' $TODAY_LOG_FILE latest.log > new.log
-cat new.log >> $TODAY_LOG_FILE
+echo "> Retrieving latest log entries from /var/log/access.log and writing to ${TODAY_ACCESS_LOG}"
+cat /var/log/access.log | grep $(date +%d/%b/%Y:) > ./access-latest.log
+diff --changed-group-format='%>' --unchanged-group-format='' $TODAY_ACCESS_LOG access-latest.log > access-new.log
+cat access-new.log >> $TODAY_ACCESS_LOG
 
-# Upload new log entries to logz.io using cURL.
+# Compare latest drupal log entries with existing entries for today and add in newest entries.
+echo "> Retrieving latest log entries from /app/log/drupal.log and writing to ${TODAY_DRUPAL_LOG}"
+cat /app/log/drupal.log | grep $(date "+%d/%b/%Y - %H:%M") > ./drupal-latest.log
+diff --changed-group-format='%>' --unchanged-group-format='' $TODAY_ACCESS_LOG drupal-latest.log > drupal-new.log
+cat drupal-new.log >> $TODAY_ACCESS_LOG
+
+# Upload new access and drupal log entries to logz.io using cURL.
 echo "> Shipping latest log entries from /var/log/access.log to Logz.io using cURL"
-curl -T new.log https://listener.logz.io:8022/file_upload/${LOGZ_TOKEN}/nginx
-rm latest.log new.log
+curl -T access-new.log https://listener.logz.io:8022/file_upload/${LOGZ_TOKEN}/nginx
+curl -T drupal-new.log https://listener.logz.io:8022/file_upload/${LOGZ_TOKEN}/http-bulk
 
-# Delete yesterdays log file if it exists.
+# Clean up temporary log files.
+rm access-latest.log access-new.log drupal-latest.log drupal-new.log
+
+# Delete yesterdays log files.
 YESTERDAY_DATE=$(date --date="yesterday" +%Y-%m-%d)
-YESTERDAY_LOG_FILE=${YESTERDAY_DATE}-access.log
-echo "> Deleting ${YESTERDAY_LOG_FILE} if it exists ..."
-if [ -f ./${YESTERDAY_LOG_FILE} ]; then
-    rm ${YESTERDAY_LOG_FILE}
-    echo "${YESTERDAY_LOG_FILE} deleted"
+YESTERDAY_ACCESS_LOG=${YESTERDAY_DATE}-access.log
+YESTERDAY_DRUPAL_LOG=${YESTERDAY_DATE}-drupal.log
+
+echo "> Deleting yesterday's log files ..."
+
+if [ -f ./${YESTERDAY_ACCESS_LOG} ]; then
+    rm ${YESTERDAY_ACCESS_LOG}
+    echo "${YESTERDAY_ACCESS_LOG} deleted"
 else
-    echo "${YESTERDAY_LOG_FILE} does not exist"
+    echo "${YESTERDAY_ACCESS_LOG} does not exist"
+fi
+
+if [ -f ./${YESTERDAY_DRUPAL_LOG} ]; then
+    rm ${YESTERDAY_DRUPAL_LOG}
+    echo "${YESTERDAY_DRUPAL_LOG} deleted"
+else
+    echo "${YESTERDAY_DRUPAL_LOG} does not exist"
 fi
 
 exit 0
