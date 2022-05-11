f43ccdc8e8a2e83e9b1b93ecea983059ba325ac5
Neil Blair <neil.r.blair@gmail.com>
2022-05-11 13:21:09 +0100

    shellcheck and google's bash style guide recommend quoting all variables containing strings

--- logging/scripts/shiplog.sh
+++ logging/scripts/shiplog.sh
@@ -9,7 +9,7 @@
 echo "Shipping log ..."
 
 # LOGZ_TOKEN environment variable is required for this script to run.
-if [ -z $LOGZ_TOKEN ]; then
+if [ -z "$LOGZ_TOKEN" ]; then
     echo "LOGZ_TOKEN is not set - exiting"
     exit 1
 fi
@@ -33,15 +33,15 @@ if [ -z "$LOG_NAME" ] || [ -z "$LOG_PATH" ] || [ -z "$LOG_DATE_PATTERN" ] || [ -
     exit 1
 fi
 
-if [ -f $LOG_PATH ]; then
+if [ -f "$LOG_PATH" ]; then
 
     echo "Shipping today's newest log entries from $LOG_PATH ..."
 
     # Create today's log file.
     todays_log="${TODAY_DATE}-${LOG_NAME}.log"
     echo "Creating ${todays_log} ..."
-    if [ ! -f ./${todays_log} ]; then
-        touch ./${todays_log}
+    if [ ! -f ./"${todays_log}" ]; then
+        touch ./"${todays_log}"
         echo "${todays_log} created"
     else
         echo "${todays_log} already exists"
@@ -52,8 +52,8 @@ if [ -f $LOG_PATH ]; then
 
     echo "Deleting ${yesterdays_log} ..."
 
-    if [ -f ./${yesterdays_log} ]; then
-        rm ${yesterdays_log}
+    if [ -f ./"${yesterdays_log}" ]; then
+        rm "${yesterdays_log}"
         echo "${yesterdays_log} deleted"
     else
         echo "${yesterdays_log} does not exist"
@@ -61,15 +61,15 @@ if [ -f $LOG_PATH ]; then
 
     # Get latest log entries and ship to logz.io.
     echo "Retrieving latest log entries from ${LOG_PATH} and writing to ${todays_log}"
-    cat $LOG_PATH | grep "$LOG_DATE_PATTERN" > ./$LOG_NAME-latest.log
-    diff --changed-group-format='%>' --unchanged-group-format='' $todays_log $LOG_NAME-latest.log > $LOG_NAME-new.log
-    cat $LOG_NAME-new.log >> $todays_log
+    cat "$LOG_PATH" | grep "$LOG_DATE_PATTERN" > ./"$LOG_NAME"-latest.log
+    diff --changed-group-format='%>' --unchanged-group-format='' "$todays_log" "$LOG_NAME-latest.log" > "$LOG_NAME-new.log"
+    cat "$LOG_NAME-new.log" >> "$todays_log"
     echo "Shipping latest log entries from ${todays_log} to Logz.io using cURL"
-    http_response=$(curl -T $LOG_NAME-new.log -s -w "%{response_code}" https://listener.logz.io:8022/file_upload/${LOGZ_TOKEN}/${LOG_TYPE})
+    http_response=$(curl -T "$LOG_NAME-new.log" -s -w "%{response_code}" https://listener.logz.io:8022/file_upload/"${LOGZ_TOKEN}"/"${LOG_TYPE}")
     exit_code=$?
 
     # Clean up temporary log files.
-    rm $LOG_NAME-latest.log $LOG_NAME-new.log
+    rm "$LOG_NAME-latest.log" "$LOG_NAME-new.log"
 
     if [ "$exit_code" != "0" ]; then
         echo "The cURL command failed with: $exit_code"
