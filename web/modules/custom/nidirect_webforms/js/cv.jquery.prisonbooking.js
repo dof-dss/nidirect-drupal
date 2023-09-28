eb81046d91f656d7c9800a333a01056702247040
Neil Blair <52457988+neilblair@users.noreply.github.com>
2023-09-28 16:50:38 +0100

    Update cv.jquery.prisonbooking.js

--- web/modules/custom/nidirect_webforms/js/cv.jquery.prisonbooking.js
+++ web/modules/custom/nidirect_webforms/js/cv.jquery.prisonbooking.js
@@ -11,6 +11,7 @@
     attach: function (context, settings) {
 
       const prisonVisitForm = once('prisonVisitForm', 'form.webform-submission-prison-visit-online-booking-form', context);
+      const $visitorRememberToggle = $('input[name="yes_remember_these_visitor_details"]', prisonVisitForm);
 
       let $prisonVisitOrderNumber = $(prisonVisitForm).find('input[name="visitor_order_number"]');
       let visitOrderVisitTypeKey = drupalSettings.prisonVisitBooking.booking_ref.visit_type_key;
@@ -44,7 +45,6 @@
         ]
       });
 
-
       const $weekSlots = $('[data-webform-key^="slots_week"]', prisonVisitForm);
       if ($weekSlots.length === 1) {
         $weekSlots.prop("open", true);
@@ -54,21 +54,70 @@
       }
 
       const $timeSlots = $('input[type="checkbox"]', $weekSlots);
-      if ($timeSlots.length) {
-        let timeSlotLimit = (visitOrderVisitTypeKey === 'E') ? 5 : 3;
-        $('[data-webform-key^="slots_week"] input[type="checkbox"]', prisonVisitForm).on('change', function(e) {
-          let timeSlotCheckedCount = $timeSlots.filter(':checked').length;
-          if (timeSlotCheckedCount === timeSlotLimit) {
-            $timeSlots.filter(':not(:checked)').prop('disabled', true);
-          }
-          else if (timeSlotCheckedCount < timeSlotLimit) {
-            $timeSlots.filter(':not(:checked)').prop('disabled', false);
-          }
-        });
+      const timeSlotLimit = (visitOrderVisitTypeKey === 'E') ? 5 : 3;
+
+      disableCheckboxes($timeSlots, timeSlotLimit);
+
+      $('[data-webform-key^="slots_week"] input[type="checkbox"]', prisonVisitForm).on('change', function(e) {
+        disableCheckboxes($timeSlots, timeSlotLimit);
+      });
+
+      function disableCheckboxes($checkboxes, checked_limit = 3) {
+        let checkboxesCheckedCount = $checkboxes.filter(':checked').length;
+        if (checkboxesCheckedCount === checked_limit) {
+          $checkboxes.filter(':not(:checked)').prop('disabled', true);
+        }
+        else if (checkboxesCheckedCount < checked_limit) {
+          $checkboxes.filter(':not(:checked)').prop('disabled', false);
+        }
       }
 
+      function save_additional_visitors(ev){
+        $("#edit-additional-visitor-details input[name^='visitor_']").each(function(index, $input){
+          localStorage.setItem('pvb.' + $input.name, $input.value);
+        });
+        return true;
+      }
 
+      function load_additional_visitors(){
+        let keys = Object.keys(localStorage);
+        for (let i= 0; i < keys.length; i++){
+          let key = keys[i];
+          if (!key.startsWith('pvb.')){
+            continue;
+          }
+          $("[name='" + key.split(".")[1] + "']").val(localStorage[key]);
+        }
+      }
 
+      function reset_additional_visitors(ev){
+        $("#edit-additional-visitor-details input[name^='visitor_']").each(function(index, $input){
+          localStorage.removeItem('pvb.' + $input.name);
+        });
+        return true;
+      }
+
+      function visitor_settings(ev) {
+        if ($visitorRememberToggle.length) {
+          if ($visitorRememberToggle.is(':checked')) {
+            save_additional_visitors();
+          } else {
+            reset_additional_visitors();
+          }
+        }
+
+        return true;
+      }
+
+      if ($visitorRememberToggle.length) {
+        $visitorRememberToggle.on('change', visitor_settings);
+      }
+
+      $(document).on('submit', visitor_settings);
+      load_additional_visitors();
     }
   };
+
+
 })(jQuery, Drupal, once);
+
