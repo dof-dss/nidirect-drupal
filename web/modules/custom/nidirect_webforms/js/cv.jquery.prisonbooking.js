964d8125cf0d528ab7bc17c93871a32cc1201f5f
Neil Blair <52457988+neilblair@users.noreply.github.com>
2023-10-17 17:46:56 +0100

    jquery changes
    Ensure jquery works for ajax enabled webform.  And since determination of available slots is now purely done server-side, have to remove some of the validation logic looking for first and last available slots.

--- web/modules/custom/nidirect_webforms/js/cv.jquery.prisonbooking.js
+++ web/modules/custom/nidirect_webforms/js/cv.jquery.prisonbooking.js
@@ -1,8 +1,6 @@
 /**
  * @file
- * JQuery to look for pages that contain the feedback webform and other wedforms.
- * If other webforms are found then the ids of the submit buttons are updated
- * to make sure that they are unique.
+ * JQuery for prison visit booking webform.
  */
 
 (function($, Drupal, once) {
@@ -11,17 +9,14 @@
     attach: function (context, settings) {
 
       const prisonVisitForm = once('prisonVisitForm', 'form.webform-submission-prison-visit-online-booking-form', context);
-      const $visitorRememberToggle = $('input[name="yes_remember_these_visitor_details"]', prisonVisitForm);
 
-      let $prisonVisitOrderNumber = $(prisonVisitForm).find('input[name="visitor_order_number"]');
-      let visitOrderVisitTypeKey = drupalSettings.prisonVisitBooking.booking_ref.visit_type_key;
-      let visitPrisons = drupalSettings.prisonVisitBooking.prisons;
-      let visitTypes = drupalSettings.prisonVisitBooking.visit_type;
-      let visitAdvanceNotice = drupalSettings.prisonVisitBooking.visit_advance_notice;
-      let visitBookingRefValidityPeriodDays = drupalSettings.prisonVisitBooking.booking_reference_validity_period_days;
-      let visitSlotsAvailable = drupalSettings.prisonVisitBooking.visit_slots;
-      let visitSequenceAffiliations = drupalSettings.prisonVisitBooking.visit_order_number_categories;
-      let visitBookingRefMaxAdvancedIssue = drupalSettings.prisonVisitBooking.visit_order_number_max_advance_issue;
+      const $prisonVisitOrderNumber = $(prisonVisitForm).find('input[name="visitor_order_number"]');
+      const visitTypeKey = drupalSettings.prisonVisitBooking.booking_ref.visit_type_key;
+      const visitPrisons = drupalSettings.prisonVisitBooking.prisons;
+      const visitTypes = drupalSettings.prisonVisitBooking.visit_type;
+      const visitAdvanceNotice = drupalSettings.prisonVisitBooking.visit_advance_notice;
+      const visitBookingRefValidityPeriodDays = drupalSettings.prisonVisitBooking.booking_reference_validity_period_days;
+      const visitBookingRefMaxAdvancedIssue = drupalSettings.prisonVisitBooking.visit_order_number_max_advance_issue;
 
       $prisonVisitOrderNumber.rules( "add", {
         validPrisonVisitBookingRef: [
@@ -30,8 +25,6 @@
           visitTypes,
           visitAdvanceNotice,
           visitBookingRefValidityPeriodDays,
-          visitSlotsAvailable,
-          visitSequenceAffiliations
         ],
         validVisitBookingRefDate: [
           true,
@@ -39,13 +32,12 @@
           visitTypes,
           visitAdvanceNotice,
           visitBookingRefValidityPeriodDays,
-          visitSlotsAvailable,
-          visitSequenceAffiliations,
           visitBookingRefMaxAdvancedIssue
         ]
       });
 
-      const $weekSlots = $('[data-webform-key^="slots_week"]', prisonVisitForm);
+      let $weekSlots = $(once('prisonVisitSlots', '[data-webform-key^="slots_week"]', context));
+
       if ($weekSlots.length === 1) {
         $weekSlots.prop("open", true);
         $('summary', $weekSlots)
@@ -53,13 +45,15 @@
           .prop('aria-pressed', true);
       }
 
-      const $timeSlots = $('input[type="checkbox"]', $weekSlots);
-      const timeSlotLimit = (visitOrderVisitTypeKey === 'V') ? 5 : 3;
+      $weekSlots.each(function() {
+        const $timeSlots = $('input[type="checkbox"]', $(this));
+        const timeSlotLimit = (visitTypeKey === 'V') ? 5 : 3;
 
-      disableCheckboxes($timeSlots, timeSlotLimit);
-
-      $('[data-webform-key^="slots_week"] input[type="checkbox"]', prisonVisitForm).on('change', function(e) {
         disableCheckboxes($timeSlots, timeSlotLimit);
+
+        $timeSlots.on('change', function(e) {
+          disableCheckboxes($timeSlots, timeSlotLimit);
+        });
       });
 
       function disableCheckboxes($checkboxes, checked_limit = 3) {
@@ -71,53 +65,8 @@
           $checkboxes.filter(':not(:checked)').prop('disabled', false);
         }
       }
-
-      function save_additional_visitors(ev){
-        $("#edit-additional-visitor-details input[name^='visitor_']").each(function(index, $input){
-          localStorage.setItem('pvb.' + $input.name, $input.value);
-        });
-        return true;
-      }
-
-      function load_additional_visitors(){
-        let keys = Object.keys(localStorage);
-        for (let i= 0; i < keys.length; i++){
-          let key = keys[i];
-          if (!key.startsWith('pvb.')){
-            continue;
-          }
-          $("[name='" + key.split(".")[1] + "']").val(localStorage[key]);
-        }
-      }
-
-      function reset_additional_visitors(ev){
-        $("#edit-additional-visitor-details input[name^='visitor_']").each(function(index, $input){
-          localStorage.removeItem('pvb.' + $input.name);
-        });
-        return true;
-      }
-
-      function visitor_settings(ev) {
-        if ($visitorRememberToggle.length) {
-          if ($visitorRememberToggle.is(':checked')) {
-            save_additional_visitors();
-          } else {
-            reset_additional_visitors();
-          }
-        }
-
-        return true;
-      }
-
-      if ($visitorRememberToggle.length) {
-        $visitorRememberToggle.on('change', visitor_settings);
-      }
-
-      $(document).on('submit', visitor_settings);
-      load_additional_visitors();
     }
   };
-
 
 })(jQuery, Drupal, once);
 
--- web/modules/custom/nidirect_webforms/js/cv.prisonbookingref.method.js
+++ web/modules/custom/nidirect_webforms/js/cv.prisonbookingref.method.js
@@ -1,6 +1,6 @@
 /**
  * @file
- * jquery-validation method for prison booking reference number.
+ * jquery-validation methods for prison booking reference number.
  *
  */
 (function ($) {
@@ -12,9 +12,7 @@
     const pvbID = value.slice(0, 2),
           pvbTypeID = value.slice(2, 3),
           pvbWeek = parseInt(value.slice(3, 5)),
-          pvbYear = parseInt(value.slice(5, 7)),
-          pvbPrisonName = params[1][pvbID],
-          pvbType = params[2][pvbTypeID];
+          pvbYear = parseInt(value.slice(5, 7));
 
     if (Object.keys(params[1]).includes(pvbID) !== true) {
       bookRefIsValid = false;
@@ -24,12 +22,6 @@
     if (Object.keys(params[2]).includes(pvbTypeID) !== true) {
       bookRefIsValid = false;
       console.log(`pvbType ${pvbTypeID} is not a valid type identifier`);
-    }
-
-    if (Object.keys(params[5][pvbPrisonName]).includes(pvbType) !== true) {
-      console.log(Object.keys(params[5][pvbPrisonName]));
-      bookRefIsValid = false;
-      console.log(`pvbType ${pvbTypeID} is not a valid type identifier for ${pvbPrisonName}`);
     }
 
     // Validate order number week and year.
@@ -52,21 +44,11 @@
           pvbTypeID = value.slice(2, 3),
           pvbWeek = parseInt(value.slice(3, 5)),
           pvbYear = parseInt(value.slice(5, 7)),
-          pvbSeq = parseInt(value.slice(8, 12)),
           pvbRefValidityDays = parseInt(params[4][pvbTypeID]),
           pvbAdvanceNoticeHours = parseInt(params[3][pvbTypeID]),
-          pvbRefMaxAdvanceIssueWeeks = parseInt(params[7]),
+          pvbRefMaxAdvanceIssueWeeks = parseInt(params[5]),
           pvbPrisonName = params[1][pvbID],
-          pvbType = params[2][pvbTypeID],
-          pvbIsIntegrated = (
-            pvbSeq >= params[6]['integrated'][0][0] && pvbSeq <= params[6]['integrated'][0][1]
-          ),
-          pvbIsAffiliationOne = (
-            pvbSeq >= params[6]['separates'][0][0] && pvbSeq <= params[6]['separates'][0][1]
-          ),
-          pvbIsAffiliationTwo = (
-            pvbSeq >= params[6]['separates'][1][0] && pvbSeq <= params[6]['separates'][1][1]
-          );
+          pvbType = params[2][pvbTypeID];
 
     // Validate booking reference number has not expired.
     const today = new Date();
@@ -84,121 +66,43 @@
     bookRefValidTo.setDate(bookRefValidFrom.getDate() + (pvbRefValidityDays - 1));
     bookRefValidTo.setHours(23, 59, 59); // Ensure valid till midnight.
 
-    // Earliest and latest date and time a booking can be made.
-    // Need to get available slots.
-    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-    let slotsAvailable = params[5][pvbPrisonName][pvbType];
-    let timeSlotsAvailable = [];
-    let firstTime = null;
-    let lastTime = null;
-    let lastDay = null;
+    // A booking ref can be issued a number of weeks in advance of
+    // valid from date.
+    const earliestBookingDate = new Date();
+    earliestBookingDate.setTime(bookRefValidFrom.getTime());
+    earliestBookingDate.setDate(earliestBookingDate.getDate() - (7 * pvbRefMaxAdvanceIssueWeeks));
 
-    if (slotsAvailable && Object.keys(slotsAvailable).length) {
-      // For each day's worth of available slots...
-      Object.keys(slotsAvailable).forEach((day, index) => {
+    // Latest date and time for booking the last time slot within the
+    // booking reference's validity period.
+    const latestBookingDate = new Date();
+    latestBookingDate.setTime(bookRefValidTo.getTime());
+    latestBookingDate.setDate(latestBookingDate.getDate() - (pvbAdvanceNoticeHours / 24));
 
-        if (slotsAvailable[day]) {
-
-          // Assume this is the last day.
-          lastDay = day;
-
-          // Are there integrated time slots?
-          if (pvbIsIntegrated && typeof slotsAvailable[day]['integrated'] !== 'undefined') {
-            timeSlotsAvailable = Object.keys(slotsAvailable[day]['integrated']);
-          }
-          // Or are there separates time slots?
-          else if ((pvbIsAffiliationOne || pvbIsAffiliationTwo) && typeof slotsAvailable[day]['separates'] !== 'undefined') {
-
-            const filterAm = function(time) {
-              let timeHours = parseInt(time.slice(0,2));
-              return timeHours <= 12;
-            }
-
-            const filterPm = function(time) {
-              let timeHours = parseInt(time.slice(0,2));
-              return timeHours > 12;
-            }
-
-            // For even numbered weeks...
-            if (today.getUTCWeekNumber() % 2 === 0) {
-              // Separates affiliation 1 get AM slots and Separates affiliation 2 get PM slots.
-              if (pvbIsAffiliationOne) {
-                timeSlotsAvailable = Object.keys(slotsAvailable[day]['separates']).filter(filterAm);
-              }
-              else {
-                timeSlotsAvailable = Object.keys(slotsAvailable[day]['separates']).filter(filterPm);
-              }
-            }
-            else {
-              // Separates 1 get PM slots and Separates 2 get AM slots.
-              if (pvbIsAffiliationOne) {
-                timeSlotsAvailable = Object.keys(slotsAvailable[day]['separates']).filter(filterPm);
-              }
-              else {
-                timeSlotsAvailable = Object.keys(slotsAvailable[day]['separates']).filter(filterAm);
-              }
-            }
-          }
-          else {
-            timeSlotsAvailable = Object.keys(slotsAvailable[day]);
-          }
-        }
-
-      });
-
-      // First and last timeslots
-      firstTime = timeSlotsAvailable[0];
-      lastTime = timeSlotsAvailable[timeSlotsAvailable.length - 1];
-
-      // A booking ref can be issued a number of weeks in advance of its
-      // valid from date.
-      const earliestBookingDate = new Date();
-      earliestBookingDate.setTime(bookRefValidFrom.getTime());
-      earliestBookingDate.setDate(earliestBookingDate.getDate() - (7 * pvbRefMaxAdvanceIssueWeeks));
-      earliestBookingDate.setHours(parseInt(firstTime.slice(0,2)), parseInt(firstTime.slice(3,5)), 0);
-
-      // Latest date and time for booking the last time slot within the
-      // booking reference's validity period.
-      const latestBookingDate = new Date();
-      latestBookingDate.setTime(bookRefValidTo.getTime());
-      latestBookingDate.setDate(latestBookingDate.getDate() - 7 + (days.indexOf(lastDay) + 1) - (pvbAdvanceNoticeHours / 24));
-      latestBookingDate.setHours(parseInt(lastTime.slice(0,2)), parseInt(lastTime.slice(3,5)), 0);
-
-      if (today.getTime() < earliestBookingDate.getTime()) {
-        bookRefIsValid = false;
-        console.log(`Visit reference number cannot be used until ${earliestBookingDate.toString()}.`);
-      }
-
-      if (today.getTime() > latestBookingDate.getTime()) {
-        bookRefIsValid = false;
-        console.log(`Advanced notice of ${pvbAdvanceNoticeHours} is required and cannot be met.`);
-      }
-
-      if (today.getTime() > bookRefValidTo.getTime()) {
-        bookRefIsValid = false;
-        console.log(`Booking reference number expired ${bookRefValidTo.toString()}`);
-      }
-
-      console.log(`######################################################`);
-      console.log(`Prison: ${pvbPrisonName}`);
-      console.log(`Visit type: ${pvbType}`);
-      console.log(`Booking date: ${today.toString()}`);
-      console.log(`Week parity: ${today.getUTCWeekNumber() % 2 === 0 ? 'even' : 'odd'}`);
-      console.log(`Booking ref validity period (days): ${pvbRefValidityDays}`);
-      console.log(`Booking ref valid from: ${bookRefValidFrom.toString()}`);
-      console.log(`Booking ref valid to: ${bookRefValidTo.toString()}`);
-      console.log(`Seq integrated? ${pvbIsIntegrated}`);
-      console.log(`Seq affil 1? ${pvbIsAffiliationOne}`);
-      console.log(`Seq affil 2? ${pvbIsAffiliationTwo}`);
-      console.log(`Last bookable time slot: ${lastDay} ${lastTime}`);
-      console.log(`Advance notice required (hours): ${pvbAdvanceNoticeHours}`);
-      console.log(`Earliest possible booking date: ${earliestBookingDate.toString()}`)
-      console.log(`Latest possible booking date: ${latestBookingDate.toString()}`);
-
-    } else {
+    if (today.getTime() < earliestBookingDate.getTime()) {
       bookRefIsValid = false;
-      console.log(`No slots available for ${pvbPrisonName} of type ${pvbType}`);
+      console.log(`Visit reference number cannot be used until ${earliestBookingDate.toString()}.`);
     }
+
+    if (today.getTime() > latestBookingDate.getTime()) {
+      bookRefIsValid = false;
+      console.log(`Advanced notice of ${pvbAdvanceNoticeHours} is required and cannot be met.`);
+    }
+
+    if (today.getTime() > bookRefValidTo.getTime()) {
+      bookRefIsValid = false;
+      console.log(`Booking reference number expired ${bookRefValidTo.toString()}`);
+    }
+
+    console.log(`######################################################`);
+    console.log(`Prison: ${pvbPrisonName}`);
+    console.log(`Visit type: ${pvbType}`);
+    console.log(`Booking date: ${today.toString()}`);
+    console.log(`Booking ref validity period (days): ${pvbRefValidityDays}`);
+    console.log(`Booking ref valid from: ${bookRefValidFrom.toString()}`);
+    console.log(`Booking ref valid to: ${bookRefValidTo.toString()}`);
+    console.log(`Advance notice required (hours): ${pvbAdvanceNoticeHours}`);
+    console.log(`Earliest possible booking date: ${earliestBookingDate.toString()}`)
+    console.log(`Latest possible booking date: ${latestBookingDate.toString()}`);
 
     return bookRefIsValid;
   }, `Visit reference number is not recognised or has expired.`);
