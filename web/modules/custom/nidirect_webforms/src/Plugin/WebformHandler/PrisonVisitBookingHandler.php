be17fa2afb834389c155c4008483b27ad5be3263
Neil Blair <52457988+neilblair@users.noreply.github.com>
2024-05-09 18:23:35 +0100

    Update prison booking webform
    Update prisonbooking.js
    
    - Reset timeslots when settings.prisonVisitBooking.resetTimeslots === true
    - Some other bugfixes and refactoring
    
    Update PrisonVisitBookingHandler.php
    
    Set a flag for JS to reset timeslot preferences and also have to clear form state and element default values / data serverside as well.

--- web/modules/custom/nidirect_webforms/src/Plugin/WebformHandler/PrisonVisitBookingHandler.php
+++ web/modules/custom/nidirect_webforms/src/Plugin/WebformHandler/PrisonVisitBookingHandler.php
@@ -136,18 +136,31 @@ class PrisonVisitBookingHandler extends WebformHandlerBase {
     // Show available timeslots in the form.
     if ($page === 'visit_preferred_day_and_time' && !empty($this->bookingReference)) {
 
-      // D8NID-1661
-      // Set hidden input current_visitor_order_number to enable
-      // comparison with the last_visitor_order_number set by JS
-      // after user chooses timeslots. This allows JS to clear any
-      // existing timeslot preferences.
+      // Reset timeslots when user changes visit_order_number.
+      // Some of the logic for setting timeslot preferences is handled
+      // via clientside JS.  Flag to clientside when timeslots must be
+      // reset.
+      $form['#attached']['drupalSettings']['prisonVisitBooking']['resetTimeslots'] = FALSE;
 
-      if ($form_state->getValue('visitor_order_number')) {
-        $form_state->setValue('current_visitor_order_number', $form_state->getValue('visitor_order_number'));
-        $webform_submission->setElementData('current_visitor_order_number', $form_state->getValue('visitor_order_number'));
-        $elements['current_visitor_order_number']['#value'] =  $form_state->getValue('visitor_order_number');
-        $elements['current_visitor_order_number']['#default_value'] =  $form_state->getValue('visitor_order_number');
+      // Check last_visitor_order_number. If different from the present
+      // visitor_order_number then timeslots need reset.
+      $last_visitor_order_number = $form_state->get('last_visitor_order_number');
+      if (!empty($last_visitor_order_number) && $last_visitor_order_number !== $form_state->getValue('visitor_order_number')) {
+        $form['#attached']['drupalSettings']['prisonVisitBooking']['resetTimeslots'] = TRUE;
+
+        $form_values = array_filter($form_state->getValues(), function ($key) {
+          return str_contains($key, '_week_');
+        }, ARRAY_FILTER_USE_KEY);
+
+        foreach ($form_values as $element_name => $element_value) {
+          $form_state->setValue($element_name, []);
+          $elements[$element_name]['#default_value'] = [];
+          $webform_submission->setElementData($element_name, []);
+        }
       }
+
+      // Update last_visitor_order_number.
+      $form_state->set('last_visitor_order_number', $form_state->getValue('visitor_order_number'));
 
       // Get available slots and show only those slots on the form.
       $available_slots = $this->bookingReference['available_slots'];
@@ -164,6 +177,7 @@ class PrisonVisitBookingHandler extends WebformHandlerBase {
       if (!empty($available_slots)) {
 
         for ($i = 4; $i >= 1; $i--) {
+
           // Form slots for each week.
           $form_slots_week = &$form['elements']['visit_preferred_day_and_time']['slots_week_' . $i];
           $webform_submission_slots_week = $webform_submission->getWebform()->getElement('slots_week_' . $i, TRUE);
@@ -658,6 +672,7 @@ class PrisonVisitBookingHandler extends WebformHandlerBase {
    * Validate visitor one DOB.
    */
   private function validateSlotPicked(array &$form, FormStateInterface $form_state) {
+
     if ($form_state->get('current_page') !== 'visit_preferred_day_and_time') {
       return;
     }
