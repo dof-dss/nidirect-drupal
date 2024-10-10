7ed421cdc307981ac4739894fc3d90be89238da3
Johan Gant <johan.gant@gmail.com>
2024-10-10 15:35:41 +0100

    Path fixes

--- .ddev/.homeadditions/appserver_build.sh
+++ .ddev/.homeadditions/appserver_build.sh
@@ -1,36 +1,36 @@
 #!/usr/bin/env bash
 
 # Variables to indicate key settings files or directories for Drupal.
-DRUPAL_ROOT=/var/www/html
+DRUPAL_ROOT=/app/web
 DRUPAL_SETTINGS_FILE=$DRUPAL_ROOT/sites/default/settings.php
 DRUPAL_SERVICES_FILE=$DRUPAL_ROOT/sites/default/services.yml
 DRUPAL_CUSTOM_CODE=$DRUPAL_ROOT/modules/custom
 DRUPAL_CUSTOM_THEME=$DRUPAL_ROOT/themes/custom/nicsdru_nidirect_theme
 
 # If we don't have a Drupal install, download it.
-if [ ! -d "/var/www/html/core" ]; then
+if [ ! -d "/app/web/core" ]; then
   echo "Installing Drupal"
   export COMPOSER_PROCESS_TIMEOUT=600
   composer install
 fi
 
 # Create Drupal public files directory and set IO permissions.
-if [ ! -d "/var/www/html/files" ]; then
+if [ ! -d "/app/web/files" ]; then
   echo "Creating public Drupal files directory"
-  mkdir -p /var/www/html/files
-  chmod -R 0775 /var/www/html/files
+  mkdir -p /app/web/files
+  chmod -R 0775 /app/web/files
 fi
 
 # Create Drupal private file directory above web root.
-if [ ! -d "/var/www/html/private" ]; then
+if [ ! -d "/app/private" ]; then
   echo "Creating private Drupal files directory"
-  mkdir -p /var/www/html/private
+  mkdir -p /app/private
 fi
 
 if [ ! -d $DRUPAL_ROOT/sites/default/settings.local.php ]; then
   echo "Creating local Drupal settings and developent services files"
-  cp -v /var/www/html/.ddev/homeadditions/config/drupal.settings.php $DRUPAL_ROOT/sites/default/settings.local.php
-  cp -v /var/www/html/.ddev/homeadditions/config/drupal.services.yml $DRUPAL_ROOT/sites/local.development.services.yml
+  cp -v /app/.ddev/homeadditions/config/drupal.settings.php $DRUPAL_ROOT/sites/default/settings.local.php
+  cp -v /app/.ddev/homeadditions/config/drupal.services.yml $DRUPAL_ROOT/sites/local.development.services.yml
 fi
 
 # Set Simple test variables and put PHPUnit config in place.
