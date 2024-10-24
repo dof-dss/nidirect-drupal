f46c1c8d3d839a6a82d85cfbe8e79d6b524be63e
Neil Blair <52457988+neilblair@users.noreply.github.com>
2024-10-24 19:17:40 +0100

    Update prisonbooking.jquery-validate-methods.js
    Attach helper validate functions to global Drupal object so they can be used across multiple js files and from within multiple Drupal.behaviours.

--- web/modules/custom/nidirect_webforms/js/prisonbooking.jquery-validate-methods.js
+++ web/modules/custom/nidirect_webforms/js/prisonbooking.jquery-validate-methods.js
@@ -4,6 +4,72 @@
  */
 (function ($, Drupal, once) {
 
+  Drupal.pvGetAge = function(dateString) {
+    // Simple conversion of dd/mm/yyyy to yyyy-mm-dd
+    if (dateString.includes('/')) {
+      const dateParts = dateString.split("/");
+      dateString = dateParts[2] + '-' + dateParts[1] + '-' + dateParts[0] + 'T00:00';
+    }
+    const today = new Date();
+    const birthDate = new Date(dateString);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    let m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+    return age;
+  };
+
+  Drupal.pvIsUniqueVisitorId = function(existingIds = {}, visitorId, visitorElement) {
+
+    let isUnique = true;
+
+    // Check against adult visitor ids.
+    for (const [key, value] of Object.entries(existingIds)) {
+      const visitorElementName = $(visitorElement).attr('name');
+      if (value === visitorId && key !== visitorElementName) {
+        isUnique = false;
+      }
+    }
+
+    // Check against child visitor ids within the present step.
+    $('[name*="visitor_"][name$="_id"]').each(function() {
+      let key = $(this).attr('name');
+      let value = $(this).val();
+      if (value === visitorId && key !== $(visitorElement).attr('name')) {
+        isUnique = false;
+      }
+    });
+
+    return isUnique;
+  }
+
+  Drupal.pvNumberOfAdultVisitors = function() {
+    let numAdults = 0;
+    $('[name^="additional_visitor_"][name$="_dob"]').each(function(index) {
+      if (Drupal.pvGetAge($(this).val()) >= 18) {
+        numAdults++;
+      }
+    });
+    return numAdults;
+  }
+
+  Drupal.pvDaysInMonth = function(m, y) {
+    switch (m) {
+      case 1 :
+        return (y % 4 == 0 && y % 100) || y % 400 == 0 ? 29 : 28;
+      case 8 : case 3 : case 5 : case 10 :
+        return 30;
+      default :
+        return 31;
+    }
+  }
+
+  Drupal.pvIsValidDate = function(d, m, y) {
+    m = parseInt(m, 10) - 1;
+    return m >= 0 && m < 12 && d > 0 && d <= Drupal.pvDaysInMonth(m, y);
+  }
+
   Drupal.behaviors.prisonVisitValidateMethods = {
     attach: function (context, settings) {
 
@@ -97,11 +163,11 @@
         }, `Visit reference number is not recognised or has expired.`);
 
         $.validator.addMethod("uniqueVisitorId", function(value, element, params) {
-          return this.optional(element) || isUniqueVisitorId(params[1], value, element) === true;
+          return this.optional(element) || Drupal.pvIsUniqueVisitorId(params[1], value, element) === true;
         }, $.validator.format("Visitor ID has already been entered"));
 
         $.validator.addMethod("minAge", function(value, element, param) {
-          return this.optional(element) || getAge(value) >= param[1];
+          return this.optional(element) || Drupal.pvGetAge(value) >= param[1];
         }, $.validator.format("Age must be {1} years old or over"));
 
         $.validator.addMethod("maxAge", function(value, element, param) {
@@ -109,7 +175,7 @@
         }, $.validator.format("Age must be no more than {1} years old"));
 
         $.validator.addMethod("maxAdults", function(value, element, param) {
-          return this.optional(element) || numberOfAdultVisitors() > param[1];
+          return this.optional(element) || Drupal.pvNumberOfAdultVisitors() <= param[1];
         }, $.validator.format("Maximum number of adults is {1}"));
 
         $.validator.addMethod("validDate", function(value, element) {
@@ -117,81 +183,12 @@
           const year = parseInt(dateParts[2]);
           const month = parseInt(dateParts[1]);
           const day = parseInt(dateParts[0]);
-          return this.optional(element) || isValidDate(day, month, year);
+          return this.optional(element) || Drupal.pvIsValidDate(day, month, year);
         }, $.validator.format("Date must be a valid date"));
 
         $.validator.addMethod("noHtml", function(value, element) {
           return this.optional(element) || value === value.replace(/(<([^>]+)>)/gi, "");
         }, "Text must be plain text only");
-
-
-        function isUniqueVisitorId(existingIds = {}, visitorId, visitorElement) {
-
-          let isUnique = true;
-
-          // Check against adult visitor ids.
-          for (const [key, value] of Object.entries(existingIds)) {
-            const visitorElementName = $(visitorElement).attr('name');
-            if (value === visitorId && key !== visitorElementName) {
-              isUnique = false;
-            }
-          }
-
-          // Check against child visitor ids within the present step.
-          $('[name*="visitor_"][name$="_id"]').each(function() {
-            let key = $(this).attr('name');
-            let value = $(this).val();
-            if (value === visitorId && key !== $(visitorElement).attr('name')) {
-              isUnique = false;
-            }
-          });
-
-          return isUnique;
-        }
-
-        function numberOfAdultVisitors() {
-          let numAdults = 0;
-
-          $('[name^="additional_visitor_"][name$="_dob"]').each(function(index) {
-            if (getAge($(this).val()) >= 18) {
-              numAdults++;
-            }
-          });
-
-          return numAdults;
-        }
-
-        function getAge(dateString) {
-          // Simple conversion of dd/mm/yyyy to yyyy-mm-dd
-          if (dateString.includes('/')) {
-            const dateParts = dateString.split("/");
-            dateString = dateParts[2] + '-' + dateParts[1] + '-' + dateParts[0] + 'T00:00';
-          }
-          const today = new Date();
-          const birthDate = new Date(dateString);
-          let age = today.getFullYear() - birthDate.getFullYear();
-          let m = today.getMonth() - birthDate.getMonth();
-          if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-            age--;
-          }
-          return age;
-        }
-
-        function daysInMonth(m, y) {
-          switch (m) {
-            case 1 :
-              return (y % 4 == 0 && y % 100) || y % 400 == 0 ? 29 : 28;
-            case 8 : case 3 : case 5 : case 10 :
-              return 30;
-            default :
-              return 31;
-          }
-        }
-
-        function isValidDate(d, m, y) {
-          m = parseInt(m, 10) - 1;
-          return m >= 0 && m < 12 && d > 0 && d <= daysInMonth(m, y);
-        }
 
       });
 
