baf7d048a05751a024b5cfaea872f9d4d14af440
Neil Blair <52457988+neilblair@users.noreply.github.com>
2025-01-22 17:23:23 +0000

    Update PrisonerPaymentsWebformHandler.php
    - setFormElementValue() helper function
    - alter form to set prisoner_max_amount
    - validate form to check visitor_id has been nominated by prisoner_id

--- web/modules/custom/nidirect_prisons/src/Plugin/WebformHandler/PrisonerPaymentsWebformHandler.php
+++ web/modules/custom/nidirect_prisons/src/Plugin/WebformHandler/PrisonerPaymentsWebformHandler.php
@@ -3,9 +3,11 @@
 namespace Drupal\nidirect_prisons\Plugin\WebformHandler;
 
 use Drupal\Component\Utility\Xss;
+use Drupal\Core\Database\Database;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Render\Markup;
 use Drupal\webform\Plugin\WebformHandlerBase;
+use Drupal\webform\Utility\WebformFormHelper;
 use Drupal\webform\WebformInterface;
 use Drupal\webform\WebformSubmissionInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -24,6 +26,26 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * )
  */
 class PrisonerPaymentsWebformHandler extends WebformHandlerBase {
+
+  /**
+   * @var array
+   */
+  protected array $form = [];
+
+  /**
+   * @var \Drupal\Core\Form\FormStateInterface
+   */
+  protected FormStateInterface $formState;
+
+  /**
+   * @var \Drupal\webform\WebformSubmissionInterface
+   */
+  protected $webformSubmission;
+
+  /**
+   * @var array
+   */
+  protected array $elements = [];
 
   /**
    * The token manager.
@@ -47,6 +69,7 @@ class PrisonerPaymentsWebformHandler extends WebformHandlerBase {
   public function defaultConfiguration() {
     return [
       'debug' => FALSE,
+      'prisoner_maximum_payment_amount' => 100,
     ];
   }
 
@@ -97,6 +120,47 @@ class PrisonerPaymentsWebformHandler extends WebformHandlerBase {
    */
   public function alterForm(array &$form, FormStateInterface $form_state, WebformSubmissionInterface $webform_submission) {
     $this->debug(__FUNCTION__);
+
+    // Store references to form, form state, and submission for
+    // easy access.
+    $this->form = &$form;
+    $this->formState = $form_state;
+    $this->webformSubmission = $webform_submission;
+    $this->elements = WebformFormHelper::flattenElements($form);
+
+    $page = $form_state->get('current_page');
+    $elements = WebformFormHelper::flattenElements($form);
+    $webform = $webform_submission->getWebform();
+
+    if ($page === 'page_payment_amount') {
+
+      // The default maximum amount a prisoner can be paid.
+      $prisoner_max_amount = $this->configuration['prisoner_maximum_payment_amount'] ?? 100;
+
+      // The prisoner_id to be paid.
+      $prisoner_id = $form_state->getValue('prisoner_id') ?? NULL;
+
+      // Try to get the current maximum from db.
+      try {
+        $connection = Database::getConnection();
+
+        $query = $connection->select('prisoner_payment_amount', 'pp_amount')
+          ->fields('pp_amount', ['amount'])
+          ->condition('prisoner_id', $prisoner_id);
+
+        $result = $query->execute()->fetchField();
+
+        if ($result) {
+          $prisoner_max_amount = $result;
+        }
+      }
+      catch (\Exception $e) {
+        $this->getLogger('nidirect_prisons')->error('Database error: @message', ['@message' => $e->getMessage()]);
+      }
+
+
+      $this->setFormElementValue('prisoner_max_amount', $prisoner_max_amount);
+    }
   }
 
   /**
@@ -104,6 +168,37 @@ class PrisonerPaymentsWebformHandler extends WebformHandlerBase {
    */
   public function validateForm(array &$form, FormStateInterface $form_state, WebformSubmissionInterface $webform_submission) {
     $this->debug(__FUNCTION__);
+    $page = $form_state->get('current_page');
+
+    // Validate the visitor_id is nominated by prisoner_id to
+    // make payments.
+
+    if ($page === 'page_prisoner_and_visitor_id') {
+
+      $visitor_ids = [];
+      $prisoner_id = $form_state->getValue('prisoner_id');
+      $visitor_id = $form_state->getValue('visitor_id');
+
+      // Get list of visitor_ids nominated by prisoner_id.
+      $connection = \Drupal::database();
+      $query = $connection->select('prisoner_payment_nominees', 'ppn');
+      $query->fields('ppn', ['visitor_ids']);
+      $query->condition('prisoner_id', $prisoner_id);
+      $result = $query->execute()->fetchField();
+
+      if ($result) {
+        $visitor_ids = explode(',', $result);
+      }
+
+      if (!$result || !in_array($visitor_id, $visitor_ids)) {
+        // Add an error if the visitor_id is not valid for the prisoner_id.
+        $form_state->setErrorByName('visitor_id', 'Check your visitor ID is correct and the prisoner has nominated you to make payments to them');
+        $form_state->setErrorByName('prisoner_id', 'Check prisoner ID is correct');
+        //$form_state->setError($form, 'Has the prisoner nominated you to make payments to them?');
+      }
+
+    }
+
     if ($value = $form_state->getValue('element')) {
       $form_state->setErrorByName('element', $this->t('The element must be empty. You entered %value.', ['%value' => $value]));
     }
@@ -238,6 +333,22 @@ class PrisonerPaymentsWebformHandler extends WebformHandlerBase {
         '@context1' => $context1,
       ];
       $this->messenger()->addWarning($this->t('Invoked @id: @class_name:@method_name @context1', $t_args), TRUE);
+    }
+  }
+
+  /**
+   * Sets the value of a form element in $form, $form_state, and $webform_submission.
+   *
+   * @param string $element_key
+   *   The key of the element whose value you want to set.
+   * @param mixed $element_value
+   *   The value to set for the element.
+   */
+  protected function setFormElementValue(string $element_key, mixed $element_value) {
+    $this->formState->setValue($element_key, $element_value);
+    $this->webformSubmission->setElementData($element_key, $element_value);
+    if (isset($this->elements[$element_key])) {
+      $this->elements[$element_key]['#default_value'] = $element_value;
     }
   }
 
