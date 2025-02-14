03000bc67c62fe5db0ba74e4a751161ff3e62a75
Neil Blair <52457988+neilblair@users.noreply.github.com>
2025-02-14 12:22:20 +0000

    Update prisoner-payments-worldpay.js
    callback adjustments

--- web/modules/custom/nidirect_prisons/js/prisoner-payments-worldpay.js
+++ web/modules/custom/nidirect_prisons/js/prisoner-payments-worldpay.js
@@ -1,6 +1,10 @@
 (function (Drupal) {
   Drupal.behaviors.prisonerPaymentsWorldpay = {
     attach: function (context, settings) {
+
+      // This runs after request to WP
+      console.log(settings.worldpay);
+
       const options = {
         url: settings.worldpay.url,
         type: 'iframe',
@@ -16,12 +20,25 @@
       const libraryObject = new WPCL.Library();
       libraryObject.setup(options);
     },
-    handleResult: function (responseData) {
-      // Handle the response, potentially setting form values
-      // and submitting the webform programmatically.
-      console.log('Payment result:', responseData);
+    handleResult: function(responseData) {
 
-      //document.querySelector('[data-drupal-selector="edit-submit"]').click();
+      const wpResponse = document.querySelector('[data-drupal-selector="edit-wp-response"]');
+      wpResponse.value = JSON.stringify(responseData);
+
+      const status = responseData.order.status;
+      switch (status) {
+        case "success":
+          document.getElementById('edit-wizard-next').click();
+          break;
+        case "failure":
+          console.log(`Worldpay status failure: ${responseData}`);
+          break;
+        case "error":
+          console.log(`Worldpay status error: ${responseData}`);
+          break;
+        default:
+          console.log(`Worldpay status unknown: ${responseData}`);
+      }
     },
   };
 })(Drupal);
